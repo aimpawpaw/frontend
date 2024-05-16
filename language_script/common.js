d258d488de9bd4bf0aca3e419def3470abd44a57
DK@Macbook <iamjdg11@gmail.com>
2024-05-16 23:16:37 +0900

    preheating 5

--- language_script/common.js
+++ language_script/common.js
@@ -1,8 +1,8 @@
 //export const URL_QUERY = "http://127.0.0.1:3000/query";
 //export const URL_QUERY = "https://port-0-aimpawpaw-backend-f69b2mlh9avo22.sel4.cloudtype.app/query";
 
-// const URL_QUERY = "https://aimpawpaw.azurewebsites.net/query"
-const URL_QUERY = "http://localhost:8080/query"
+const URL_QUERY = "https://aimpawpaw.azurewebsites.net/query"
+// const URL_QUERY = "http://localhost:8080/query"
 
 
 function handleFgContainer() {
